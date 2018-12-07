f9ce917ab666bb2c8fbc04bd310eb5ef6f1faa6a
BiserBorisov <butoff@abv.bg>
2018-12-07 12:55:43 +0200

    First commit for Hackerrank Java Intro.

---
+++ Java/src/JavaStdinStdout.java
@@ -0,0 +1,20 @@
+// You must read  integers from stdin and then print them to stdout.
+// Each integer must be printed on a new line.
+
+import java.util.Scanner;
+
+public class JavaStdinStdout {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+
+            String line = scanner.nextLine();
+
+            System.out.println(line);
+        }
+
+    }
+}
